0c74b5c84d83e375b67caf9f02cb095aa1ce5a69
Uli Troyo <5466444+UliTroyo@users.noreply.github.com>
2022-10-15 12:53:37 -0400

    Minor work

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,35 @@
 import { argv } from "process";
 import { mkdirSync, existsSync } from "fs";
 import { resolve, format, parse } from "path";
+import { Readable } from "stream";
 
 // Check if BLUE received any args
 const mayContainBlue = argv.length > 2 && argv.slice(2, argv.length);
+if (!mayContainBlue)
+  throw new Error("Sorry homie, gimme some .blue files if you want my help!");
 
-if (mayContainBlue) {
-  try {
-    // Retrieve only .blue files from the args array
-    const blueFiles = mayContainBlue
-      .map(function doesItParse(maybeFile) {
-        return parse(maybeFile);
-      })
-      .filter(function isItBlue(file) {
-        return file.ext == ".blue";
-      });
-    const buildDir = resolve("build");
+try {
+  // Retrieve only .blue files from the args array
+  const blueFiles = mayContainBlue
+    .map(function doesItParse(maybeFile) {
+      return parse(maybeFile);
+    })
+    .filter(function isItBlue(file) {
+      return file.ext == ".blue";
+    });
+  const buildDir = resolve("build");
 
-    if (!existsSync(buildDir)) {
-      mkdirSync(buildDir);
-    }
-
-    // Only outputs to a build/ dir inside the current dir
-    for (const file of blueFiles) {
-      await Bun.write(
-        `build/${file.base}.html`,
-        Bun.file(format({ dir: file.dir, name: file.name }))
-      );
-    }
-  } catch (err) {
-    console.error(err.message);
+  if (!existsSync(buildDir)) {
+    mkdirSync(buildDir);
   }
-} else {
-  console.error("Sorry homie, give me some .blue files if you want my help!");
+
+  // Only outputs to a build/ dir inside the current dir
+  for (const file of blueFiles) {
+    await Bun.write(
+      `build/${file.base}.html`,
+      Bun.file(format({ dir: file.dir, name: file.name }))
+    );
+  }
+} catch (err) {
+  console.error(err.message);
 }
